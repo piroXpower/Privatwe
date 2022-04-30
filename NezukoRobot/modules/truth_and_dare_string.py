4251c71bb731ea51d02fd3451ee735e7f56a8b83
Hiro-Kun <92907462+DarlingHiro@users.noreply.github.com>
2022-04-30 21:01:37 +0530

    Add files via upload

---
+++ NezukoRobot/modules/truth_and_dare_string.py
@@ -0,0 +1,177 @@
+TRUTH = (
+    "Have you ghosted someone?" "Have you ever walked in on your parents doing 'it'?",
+    "Who was the last person you liked the most? Why?",
+    "Have you ever been suspended from school?",
+    "If you had to choose between going naked or having your thoughts appear in thought bubbles above your head for everyone to read, which would you choose?",
+    "What’s the one thing you’re afraid to lose?",
+    "Do you like someone as of the moment?",
+    "One thing about your best friend you are jealous of?",
+    "Would you cheat on your boyfriend for a rich guy?",
+    "What is your biggest turn on?",
+    "When’s the last time you lied to your parents and why?",
+    "Describe your ideal partner.",
+    "What’s the scariest thing you’ve ever done?",
+    "Have you ever picked your nose and eaten it?",
+    "When’s the last time you lied to your parents and why?",
+    "Have you ever lied about your age to participate in a contest?",
+    "Have you ever been caught checking someone out?",
+)
+
+DARE = (
+    "Show the most embarrassing photo on your phone"
+    "Show the last five people you texted and what the messages said",
+    "Let the rest of the group DM someone from your Instagram account",
+    "Eat a raw piece of garlic",
+    "Do 100 squats",
+    "Keep three ice cubes in your mouth until they melt",
+    "Say something dirty to the person on your leftYou've got company!",
+    "Give a foot massage to the person on your right",
+    "Put 10 different available liquids into a cup and drink it",
+    "*Yell out the first word that comes to your mind",
+    "Give a lap dance to someone of your choice",
+    "Remove four items of clothing",
+    "Like the first 15 posts on your Facebook newsfeed",
+    "Eat a spoonful of mustard",
+    "Keep your eyes closed until it's your go again",
+    "Send a sext to the last person in your phonebook",
+    "Show off your orgasm face",
+    "Seductively eat a banana",
+    "Empty out your wallet/purse and show everyone what's inside",
+    "Do your best sexy crawl",
+    "Pretend to be the person to your right for 10 minutes",
+    "Eat a snack without using your hands",
+    "Say two honest things about everyone else in the group",
+    "Twerk for a minute",
+    "Try and make the group laugh as quickly as possible",
+    "Try to put your whole fist in your mouth",
+    "Tell everyone an embarrassing story about yourself",
+    "Try to lick your elbow",
+    "Post the oldest selfie on your phone on Instagram Stories",
+    "Tell the saddest story you know",
+    "Howl like a luinor for two minutes",
+    "Dance without music for two minutes",
+    "Pole dance with an imaginary pole",
+    "Let someone else tickle you and try not to laugh",
+    "Put as many snacks into your mouth at once as you can",
+    "Send your most recent selfie.",
+    "Send your ugliest selfie.",
+    "Send a screenshot of your facebook search history",
+    "Send a screenshot of your gallery.",
+    "Send a screenshot of your messenger inbox",
+    "Tell something very intimate.",
+    "Send a screenshot of your twitter inbox",
+    "Send a screenshot of your homescreen.",
+    "Send a cover of your favorite song. 🎤",
+    "Do a lyric prank on someone and send proof.",
+    "Confess to your current crush. ❤️",
+    "Declare who is your true love.",
+    "Send a screenshot of your gallery.",
+    "Set your crush’s picture as your dp.",
+    "Suggest me more dares.",
+)
+SIGMA = (
+    "https://te.legra.ph/file/4b69fbe424844911b8918.mp4",
+    "https://te.legra.ph/file/cdb29d414660966a13c1d.mp4",
+    "https://te.legra.ph/file/3692afc4a4c220cddd143.mp4",
+    "https://te.legra.ph/file/9bec36decc0fa670b0972.mp4",
+    "https://te.legra.ph/file/121f5914ec63a093833d4.mp4",
+    "https://te.legra.ph/file/6da80542da78694e72772.mp4",
+    "https://te.legra.ph/file/7612481a412ba8d6526f9.mp4",
+    "https://te.legra.ph/file/c6fd080571a3184cf70fd.mp4",
+    "https://te.legra.ph/file/d42c92ba73e4d6597cdaf.mp4",
+    "https://te.legra.ph/file/26cf83b7e0c4919a7387f.mp4",
+    "https://te.legra.ph/file/966efafa8444612ce17eb.mp4",
+    "https://te.legra.ph/file/67d5b7bf76568da737bb3.mp4",
+    "https://te.legra.ph/file/8bd84143da145f22efe7e.mp4",
+    "https://te.legra.ph/file/5ef3b05e62a3d79bc134c.mp4",
+    "https://te.legra.ph/file/9059707b626f16512902c.mp4",
+    "https://te.legra.ph/file/eb8fd689e5bf148a3bccd.mp4",
+    "https://te.legra.ph/file/7ae9f34f7bab75c62721a.mp4",
+    "https://te.legra.ph/file/74c869332615283cbfcec.mp4",
+    "https://te.legra.ph/file/18080231bf03cad0e36fb.mp4",
+    "https://te.legra.ph/file/36b067368cbf3a506c42a.mp4",
+    "https://te.legra.ph/file/cd0319a58d40f3c4bc2d0.mp4",
+    "https://te.legra.ph/file/08a830bbcdbd8666d1fd8.mp4",
+    "https://te.legra.ph/file/3a455d09e381d736a162e.mp4",
+    "https://te.legra.ph/file/74c869332615283cbfcec.mp4",
+    "https://te.legra.ph/file/a8cde9a02b2562ddc99f5.mp4",
+    "https://te.legra.ph/file/5cb619fdc491eb9a86134.mp4",
+    "https://te.legra.ph/file/3ca0f0637f5431a422818.mp4",
+    "https://te.legra.ph/file/1e38235febfe52cdd0490.mp4",
+    "https://te.legra.ph/file/e27fd617cc4e52835cb76.mp4",
+)
+
+
+COSPLAY = (
+    "https://telegra.ph/file/c296eb3d7603a31062159.jpg",
+    "https://telegra.ph/file/baddf7835b291a7fd3cfd.jpg",
+    "https://telegra.ph/file/dfbf2aaa731e1cf00a2e1.jpg",
+    "https://telegra.ph/file/893a0c47cec218d449965.jpg",
+    "https://telegra.ph/file/5d46622a69d528737ddb9.jpg",
+    "https://telegra.ph/file/b7340cfda5e369b03df17.jpg",
+    "https://telegra.ph/file/40b6205209bbe052c30ed.jpg",
+    "https://telegra.ph/file/0b286c405bf999f858850.jpg",
+    "https://telegra.ph/file/e7d8c97856a2c52711a14.jpg",
+    "https://telegra.ph/file/68bac98751716c36509ce.jpg",
+    "https://telegra.ph/file/df9b68a7028e02cf34168.jpg",
+    "https://telegra.ph/file/af0ba847aa6127a71e45f.jpg",
+    "https://telegra.ph/file/4e54a1671655d2a90687d.jpg",
+    "https://telegra.ph/file/7842a0e654bd98b67f989.jpg",
+    "https://telegra.ph/file/1b2f171142860178072e3.jpg",
+)
+
+
+
+
+
+WAIFU = (
+   "https://telegra.ph/file/ec23f8f3631f5a4a247aa.jpg",
+   "https://telegra.ph/file/92aa2b0d4fafc81772820.jpg",
+   "https://telegra.ph/file/57b186d976b83e4bcf0ce.jpg",
+   "https://telegra.ph/file/6e9356fa15d997af52447.jpg",
+   "https://telegra.ph/file/82392ecb8b25f4f697ab7.jpg",
+   "https://telegra.ph/file/b969b06d44626fdf4f2fb.jpg",
+   "https://telegra.ph/file/cc8ca4a88843e2b35741f.jpg",
+   "https://telegra.ph/file/c92a723f12cc37b30f753.jpg",
+   "https://telegra.ph/file/cde47936b08226edf5286.jpg",
+   "https://telegra.ph/file/fd385db52cec67bc6b0d3.jpg",
+   "https://telegra.ph/file/2d5e09f3ced7e2eb01bd1.jpg",
+   "https://telegra.ph/file/d86c8d3d6387ff1e9cfde.jpg",
+   "https://telegra.ph/file/ab514ac9d1e693476a326.jpg",
+   "https://telegra.ph/file/90da2e3d3edb28fb62458.jpg",
+   "https://telegra.ph/file/6493a1047a5197824b3c7.jpg",
+   "https://telegra.ph/file/90a851837558253f2c990.jpg",
+   "https://telegra.ph/file/0e855f9c1ce19ecad5819.jpg",
+   "https://telegra.ph/file/c476a1ad86ef6dc77c16c.jpg",
+   "https://telegra.ph/file/b33c22cb82b14ad5c8e6f.jpg",
+)
+
+
+
+
+
+REELS = (
+   "https://telegra.ph/file/80404f5245a2c69520ad8.mp4",
+   "https://telegra.ph/file/a011c9f3dc5131aaf9016.mp4",
+   "https://telegra.ph/file/dfce15c447b116d3167fb.mp4",
+   "https://telegra.ph/file/9f2967807e306527904e5.mp4",
+   "https://telegra.ph/file/4f39a224d945a3ddea3e7.mp4",
+   "https://telegra.ph/file/bd5861dede7946283c567.mp4",
+   "https://telegra.ph/file/022e68d0059a3ee82a549.mp4",
+   "https://telegra.ph/file/4f42af401780fab1c7666.mp4",
+   "https://telegra.ph/file/dda9970fa9fee8171374c.mp4",
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
